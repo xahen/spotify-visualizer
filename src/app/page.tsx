0e3abc50fd961f27163533cbab42b86fbc96a759
xahen <18694387+xahen@users.noreply.github.com>
2025-03-20 14:13:17 +0100

    added data processing and refactored it into it's own function + sorted object by times_listened

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,71 @@ type SongList = {
   [key: string]: Song;
 };
 
+const processData = (
+  songs: SongList,
+  artists: ArtistList,
+  jsonContents: any
+) => {
+  Object.entries(jsonContents).forEach(([filename, data]: [string, any]) => {
+    for (const entry of data) {
+      let songName = entry["master_metadata_track_name"];
+      let artistName = entry["master_metadata_album_artist_name"];
+
+      if (songName !== null && artistName !== null) {
+        if (songName in songs) {
+          songs[songName].ms_listened += entry["ms_played"];
+          songs[songName].times_listened += 1;
+          if (entry["skipped"]) {
+            songs[songName].times_skipped;
+          }
+        } else {
+          songs[songName] = {
+            name: songName,
+            artist: artistName,
+            times_listened: 1,
+            ms_listened: entry["ms_played"],
+            times_skipped: entry["skipped"] ? 1 : 0,
+          };
+        }
+
+        if (artistName in artists) {
+          artists[artistName].ms_listened += entry["ms_played"];
+          artists[artistName].songs_played += 1;
+          if (entry["skipped"]) {
+            artists[artistName].times_skipped += 1;
+          }
+        } else {
+          artists[artistName] = {
+            name: artistName,
+            songs_played: 1,
+            ms_listened: entry["ms_played"],
+            times_skipped: entry["skipped"] ? 1 : 0,
+          };
+        }
+      }
+    }
+  });
+
+  const sortedSongs = Object.fromEntries(
+    Object.entries(songs).sort(
+      (a: [string, Song], b: [string, Song]) =>
+        b[1].times_listened - a[1].times_listened
+    )
+  ) as SongList;
+
+  Object.entries(sortedSongs).forEach(([key, value]: [string, Song]) => {
+    console.log(value);
+  });
+
+  {
+    /*
+  Object.entries(artists).forEach(([key, value]) => {
+    console.log(value);
+  });
+    */
+  }
+};
+
 export default function Home() {
   const [jsonData, setJsonData] = useState<{ [key: string]: any }>({});
 
@@ -58,60 +123,12 @@ export default function Home() {
         }
       }
 
-      let artists: ArtistList = {};
-      let songs: SongList = {};
+      const artists: ArtistList = {};
+      const songs: SongList = {};
 
       setJsonData(jsonContents);
 
-      Object.entries(jsonContents).forEach(([filename, data]) => {
-        for (let entry of data) {
-          let songName = entry["master_metadata_track_name"];
-          let artistName = entry["master_metadata_album_artist_name"];
-
-          if (songName in songs) {
-            songs[songName].ms_listened += entry["ms_played"];
-            songs[songName].times_listened += 1;
-            if (entry["skipped"]) {
-              songs[songName].times_skipped;
-            }
-          } else {
-            songs[songName] = {
-              name: songName,
-              artist: artistName,
-              times_listened: 1,
-              ms_listened: entry["ms_played"],
-              times_skipped: entry["skipped"] ? 1 : 0,
-            };
-          }
-
-          if (artistName in artists) {
-            artists[artistName].ms_listened += entry["ms_played"];
-            artists[artistName].songs_played += 1;
-            if (entry["skipped"]) {
-              artists[artistName].times_skipped += 1;
-            }
-          } else {
-            artists[artistName] = {
-              name: artistName,
-              songs_played: 1,
-              ms_listened: entry["ms_played"],
-              times_skipped: entry["skipped"] ? 1 : 0,
-            };
-          }
-        }
-      });
-
-      {
-        /*
-      Object.entries(songs).forEach(([key, value]) => {
-        console.log(value);
-      });
-      */
-      }
-
-      Object.entries(artists).forEach(([key, value]) => {
-        console.log(value);
-      });
+      processData(songs, artists, jsonContents);
     } catch (err) {
       console.error(err);
     }
