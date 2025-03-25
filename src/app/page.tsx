e5f9e94d2f52bacaa15e3776b4ce66a2e8260136
xahen <18694387+xahen@users.noreply.github.com>
2025-03-25 18:58:01 +0100

    added logos and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { StatsPage } from "./components/stats";
 import { ZipUpload } from "./components/upload";
 import { useAppContext } from "@/context/AppContext";
 
-import { FaAlignJustify, FaX } from "react-icons/fa6";
+import { FaBars, FaX, FaHouse, FaChartLine } from "react-icons/fa6";
 
 export default function Home() {
   const [uploaded, setUploaded] = useState<boolean>(false);
@@ -16,8 +16,27 @@ export default function Home() {
   const { songData, setSongData } = useAppContext();
 
   return (
-    <main className="flex">
-      <div className="h-screen w-16 bg-amber-700"><FaAlignJustify /></div>
+    <main className="flex flex-row">
+      <div
+        className={`h-screen bg-gray-900 flex flex-col transition-all duration-250 ${
+          sidebar ? "w-64" : "w-16"
+        } `}
+      >
+        <div className="top-0 h-16 w-16 flex">
+          <div className="m-auto">
+            {!sidebar && <FaBars size={35} onClick={() => setSidebar(true)} />}
+            {sidebar && <FaX size={35} onClick={() => setSidebar(false)} />}
+          </div>
+        </div>
+        <div className="h-full w-16 flex flex-col">
+          <div className="m-auto">
+            <FaHouse size={35} />
+          </div>
+          <div className="m-auto">
+            <FaChartLine size={35} />
+          </div>
+        </div>
+      </div>
       <div className="w-full h-screen overflow-x-hidden bg-slate-700 flex p-4">
         {!uploaded && (
           <div className="m-auto">
