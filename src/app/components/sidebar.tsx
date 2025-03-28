ccbc2c1d0764201426dc9ee4650d02238e0b768b
xahen <18694387+xahen@users.noreply.github.com>
2025-03-28 10:12:01 +0100

    fixed sidebar buttons visually

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { FaBars, FaX, FaHouse, FaChartLine } from "react-icons/fa6";
+import { FaBars, FaX, FaHouse, FaChartLine, FaClock } from "react-icons/fa6";
 
 export const Sidebar = () => {
   const [expanded, setExpanded] = useState(false);
@@ -11,18 +11,42 @@ export const Sidebar = () => {
         expanded ? "w-64" : "w-16"
       } `}
     >
-      <div className="top-0 h-16 w-16 flex">
+      <div className="top-0 h-16 w-16 flex hover:bg-white/20">
         <div className="m-auto">
           {!expanded && <FaBars size={35} onClick={() => setExpanded(true)} />}
           {expanded && <FaX size={35} onClick={() => setExpanded(false)} />}
         </div>
       </div>
       <div className="h-full w-16 flex flex-col">
-        <div className="m-auto">
-          <FaHouse size={35} />
+        <div className="h-16 w-64 flex hover:bg-white/20">
+          <div className="h-16 w-16 flex">
+            <FaHouse className="m-auto" size={35} />
+          </div>
+          {expanded && (
+            <div className="flex h-16 w-fit">
+              <p className="ml-2 m-auto text-2xl">Home</p>
+            </div>
+          )}
         </div>
-        <div className="m-auto">
-          <FaChartLine size={35} />
+        <div className="h-16 w-64 flex hover:bg-white/20">
+          <div className="h-16 w-16 flex">
+            <FaChartLine className="m-auto" size={35} />
+          </div>
+          {expanded && (
+            <div className="flex h-16 w-fit">
+              <p className="ml-2 m-auto text-2xl">Statistics</p>
+            </div>
+          )}
+        </div>
+        <div className="h-16 w-64 flex hover:bg-white/20">
+          <div className="h-16 w-16 flex">
+            <FaClock className="m-auto" size={35} />
+          </div>
+          {expanded && (
+            <div className="flex h-16 w-fit">
+              <p className="ml-2 m-auto text-2xl">Historic</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
