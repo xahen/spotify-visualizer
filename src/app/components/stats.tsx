745fef3c6835c05b62e8c1707ff8bdc3f80da2f7
oliver borg <18694387+xahen@users.noreply.github.com>
2025-03-29 19:51:54 +0100

    removed the console logging test buttons for cleanup

--- src/app/components/stats.tsx
+++ src/app/components/stats.tsx
@@ -14,25 +14,9 @@ type SongList = {
   [key: string]: Song;
 };
 
-const sortByListens = (songs: any) => {
+const sortSongByListens = (songs: any) => {
   const sortedSongs = Object.entries(songs).sort(
     ([, a], [, b]) => b.times_listened - a.times_listened
-  );
-
-  return sortedSongs;
-};
-
-const sortBySkips = (songs: any) => {
-  const sortedSongs = Object.entries(songs).sort(
-    ([, a], [, b]) => b.times_skipped - a.times_skipped
-  );
-
-  return sortedSongs;
-};
-
-const sortByMS = (songs: any) => {
-  const sortedSongs = Object.entries(songs).sort(
-    ([, a], [, b]) => b.ms_listened - a.ms_listened
   );
 
   return sortedSongs;
@@ -49,78 +33,11 @@ const sortArtistByListens = (songs: any) => {
 export const StatsPage = () => {
   const { songData, artistData } = useAppContext();
 
-  const sortedByListens = sortByListens(songData);
-  const sortedBySkips = sortBySkips(songData);
-  const sortedByMS = sortByMS(songData);
-
+  const sortedByListens = sortSongByListens(songData);
   const artistByListens = sortArtistByListens(artistData);
-  const artistBySkips = sortBySkips(artistData);
-  const artistByMS = sortByMS(artistData);
 
   return (
     <div className="m-auto">
-      <div className="flex justify-between">
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in sortedByListens) {
-              console.log(sortedByListens[x]);
-            }
-          }}
-        >
-          songs sorted by listens
-        </button>
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in sortedBySkips) {
-              console.log(sortedBySkips[x]);
-            }
-          }}
-        >
-          songs sorted by skips
-        </button>
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in sortedByMS) {
-              console.log(sortedByMS[x]);
-            }
-          }}
-        >
-          songs sorted by ms
-        </button>
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in artistByListens) {
-              console.log(artistByListens[x]);
-            }
-          }}
-        >
-          artists sorted by listens
-        </button>
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in artistBySkips) {
-              console.log(artistBySkips[x]);
-            }
-          }}
-        >
-          artist sorted by skips
-        </button>
-        <button
-          className="border-3 p-3 rounded-4xl text-xl hover:opacity-50 m-2"
-          onClick={() => {
-            for (let x in artistByMS) {
-              console.log(artistByMS[x]);
-            }
-          }}
-        >
-          artist sorted by ms
-        </button>
-      </div>
       <div className="flex mt-4">
         <div className="ml-25">
           <div>
