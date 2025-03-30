104a050f2224e6c2f24787d49f0f4043034355ab
oliver borg <18694387+xahen@users.noreply.github.com>
2025-03-31 01:48:42 +0200

    fixed overflow issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { WelcomePage } from "@/app/components/introduction";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen overflow-x-hidden bg-black/50 flex p-4">
+    <div className="w-full h-screen overflow-x-hidden overflow-y-auto bg-black/50 flex p-4">
       <div className="m-auto">
         <WelcomePage />
       </div>
